ebc85251f3992dc186f8ddae49a10b052ea96444
Raphael Morand <morand.raphael.13@gmail.com>
2017-04-22 13:10:21 +0200

    added isbn random generator function
    the isbn function generates a 13 digit formated isbn identity.

--- lib/faker/book.rb
+++ lib/faker/book.rb
@@ -18,6 +18,22 @@ module Faker
       def genre
         fetch('book.genre')
       end
+      
+      def isbn random
+				isbn = ''
+				breaks = [2, 3, 5, 11]
+				13.times do |n|
+					isbn += random.rand(0..9).to_s
+					if breaks.include? n
+						isbn += '-'
+					end
+				end
+				isbn
+			end
+
+      def isbn
+        isbn Random.new Time.now.to_i  
+			end      
     end
   end
 end
